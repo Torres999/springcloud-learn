3827fa86749c426a1342b90b8e4694c0476d9238
torres999 <dalintorres@icloud.com>
2017-03-24 23:55:33 +0800

    add config consume

---
+++ springcloud-learn-config-consume/src/main/java/com/example/TestController.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lilinlin on 2017/3/24.
+ */
+@RefreshScope
+@RestController
+class TestController {
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+}
