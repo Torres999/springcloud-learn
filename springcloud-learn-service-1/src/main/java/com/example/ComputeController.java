493226927112a9c85577916da3d8c00d298c7cd8
torres999 <dalintorres@icloud.com>
2017-03-31 09:54:24 +0800

    update config properties

--- springcloud-learn-service-1/src/main/java/com/example/ComputeController.java
+++ springcloud-learn-service-1/src/main/java/com/example/ComputeController.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +23,29 @@ public class ComputeController {
 
     @Autowired
     private DiscoveryClient client;
+//    @ApiOperation(value = "校验手机号码是否有效", notes = "校验手机号码唯一性", response = CheckTelResp.class, tags = {
+//            "User/inner",})
+//    @RequestMapping(value = "/user/checkTel",
+//            produces = {"application/json"},
+//            headers = "content-type=application/x-www-form-urlencoded",
+//            method = RequestMethod.POST)
+//    ResponseEntity<CheckTelResp> userCheckTelPost(
+//            @ApiParam(value = "电话号码", required = true) @RequestParam(value = "telephone", required = true) String telephone);
 
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+
+
+    @ApiOperation(value = "计算两个数字的和")
+    @ApiResponses(
+        @ApiResponse(code = 200, message = "返回计算结果", response = Integer.class)
+    )
+    @RequestMapping(value = "/add",
+            method = RequestMethod.GET,
+            produces = "application/json")
+    public Integer add(@ApiParam(value = "计算因子a", required = true)@RequestParam Integer a,
+                       @ApiParam(value = "计算因子b", required = true)@RequestParam Integer b,
+                       @RequestParam(required = false) Integer c) {
         ServiceInstance instance = client.getLocalServiceInstance();
-        Integer r = a + b;
+        Integer r = a - b;
         logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return r;
     }
--- springcloud-learn-service-1/src/main/java/com/example/SpringcloudLearnService1Application.java
+++ springcloud-learn-service-1/src/main/java/com/example/SpringcloudLearnService1Application.java
@@ -1,11 +1,21 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableDiscoveryClient
 @SpringBootApplication
+
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class SpringcloudLearnService1Application {
 
 	public static void main(String[] args) {
